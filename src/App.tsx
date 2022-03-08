34c52ea2b8f59619900dde645801e4d5f19e5b98
Austin Cory Bart <acbart@vt.edu>
2022-03-08 11:09:23 -0500

    Update packages, landing page

--- src/App.tsx
+++ src/App.tsx
@@ -4,13 +4,8 @@ import "./App.css";
 function App(): JSX.Element {
     return (
         <div className="App">
-            <header className="App-header">
-                UD CISC275 with React Hooks and TypeScript
-            </header>
-            <p>
-                Edit <code>src/App.tsx</code> and save. This page will
-                automatically reload.
-            </p>
+            <header className="App-header">Movie Records</header>
+            <p>Track who watched what!</p>
         </div>
     );
 }

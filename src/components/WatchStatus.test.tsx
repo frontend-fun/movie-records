47881524349079261e3fa127dd7588508b3ddab4
Austin Cory Bart <acbart@vt.edu>
2022-03-17 11:24:58 -0400

    Provide an example smoke test for WatchStatus

---
+++ src/components/WatchStatus.test.tsx
@@ -0,0 +1,32 @@
+import React from "react";
+import { render, screen } from "@testing-library/react";
+import { WatchStatus } from "./WatchStatus";
+
+describe("WatchStatus test cases", () => {
+    const seenWatchStatus = {
+        seen: false,
+        liked: false,
+        when: null
+    };
+    it("Renders the watch status", () => {
+        render(<WatchStatus watched={seenWatchStatus}></WatchStatus>);
+    });
+    it("Renders the watch status", () => {
+        render(<WatchStatus watched={seenWatchStatus}></WatchStatus>);
+        const watchedText = screen.getByText(/Watched/i);
+        expect(watchedText).toBeInTheDocument();
+    });
+    it("Renders the watch status", () => {
+        render(
+            <WatchStatus
+                watched={{
+                    seen: false,
+                    liked: false,
+                    when: null
+                }}
+            ></WatchStatus>
+        );
+        const watchedText = screen.getByText(/Not yet watched/i);
+        expect(watchedText).toBeInTheDocument();
+    });
+});

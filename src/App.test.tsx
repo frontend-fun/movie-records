12b39c9e8a0636843d88f974f8988a1463c229a9
Austin Cory Bart <acbart@vt.edu>
2022-05-04 12:28:23 -0400

    Bare bones example of testing the add movie modal

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,32 @@
 import React from "react";
 import { render, screen } from "@testing-library/react";
 import App from "./App";
+import userEvent from "@testing-library/user-event";
 
 test("renders the course name somewhere", () => {
     render(<App />);
     const linkElement = screen.getByText(/Movie Records/i);
     expect(linkElement).toBeInTheDocument();
+});
+
+test("the add movie modal works", () => {
+    render(<App />);
+
+    const originalHeaders = screen.getAllByRole("heading").length;
+
+    const addButton = screen.getByRole("button", { name: /Add New Movie/i });
+    addButton.click();
+    const dialog = screen.getByRole("dialog");
+
+    expect(dialog).toBeInTheDocument();
+
+    const movieIdBox = screen.getByLabelText(/YouTube ID:/i);
+    userEvent.type(movieIdBox, "1234");
+
+    const saveButton = screen.getByRole("button", { name: /Save Changes/i });
+    saveButton.click();
+
+    const newHeaders = screen.getAllByRole("heading").length;
+
+    expect(originalHeaders + 1).toEqual(newHeaders);
 });
